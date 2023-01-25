c0e24fe6edefbfd3b5dcdaa073de978a56778cd2
moldy <moldy@bookips.com>
2023-01-25 17:59:16 +0900

    set port, test

--- start/new-nest/src/main.ts
+++ start/new-nest/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Test} from "@nestjs/testing";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8520);
 }
 bootstrap();
