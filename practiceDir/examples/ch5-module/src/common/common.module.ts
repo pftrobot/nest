5923012e8326841d34b14920686cb96f5c21a63a
moldy <moldy@bookips.com>
2023-01-26 15:43:56 +0900

    global module

--- practiceDir/examples/ch5-module/src/common/common.module.ts
+++ practiceDir/examples/ch5-module/src/common/common.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import {Global, Module} from '@nestjs/common';
 import { CommonService } from './common-service';
 
+// 전역 모듈 생성
+@Global()
 @Module({
   providers: [CommonService],
   exports: [CommonService],
