d53f2fb9c328697c180af675b15cb4aaf385d44e
moldy <moldy@bookips.com>
2023-01-26 13:35:40 +0900

    new project modified

--- start/new-nest/src/app.controller.ts
+++ start/new-nest/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, HttpCode, Param, Patch, Req, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Request} from "express";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
+
+  /*
+  getHello(@Req() req: Request): string {
+    console.log('::: req ',req)
+    return this.appService.getHello()
+  }
+  */
 }
